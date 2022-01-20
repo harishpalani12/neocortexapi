eb8558666d905f62f1cf494bff2981e2e2d9ec3b
Gaurav Honnavara <gauravhm96@gmail.com>
2022-01-20 12:17:27 +0100

    ML Model Project (Sequence)

---
+++ MyProjectWork/MyMLConsoleAPP/MyMLConsoleAPP/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyMLConsoleAPP;
+
+// Add input data
+var sampleData = new MLModel1.ModelInput()
+{
+    Col0 = "This restaurant not good."
+};
+
+// Load model and predict output of sample data
+var result = MLModel1.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
