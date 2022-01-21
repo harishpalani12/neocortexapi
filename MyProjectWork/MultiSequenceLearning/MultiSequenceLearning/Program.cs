0722dc7e56eb786cd82e1a009b8197c5200629a0
Gaurav Honnavara <gauravhm96@gmail.com>
2022-01-21 17:23:58 +0100

    MultiSequenceLearning(HelperMethod)

---
+++ MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MultiSequenceLearning
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
+
---
+++ MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/SequenceLearning_HTM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiSequenceLearning
+{
+    class SequenceLearningSTM
+    {
+        static readonly int numColumns = 2048;
+
+        static readonly string LungCancerSequenceDataFile = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory + @"Final Project\Sequence\Anticancer_Peptides\ACPs_Lung_cancer.csv");
+        static readonly string BreastCancerSequenceDataFile = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory + @"Final Project\Sequence\Anticancer_Peptides\ACPs_Breast_cancer.csv");
+
+        public void InitiateCancerSequenceClassification()
+        {
+
+            int inputBits = 31;
+            int maxCycles = 30;
+
+        }
+
+
+    }
+}
