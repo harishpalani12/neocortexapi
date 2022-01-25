93745cd155e335c69e4a7cfe2f5aa5ea168d5687
Gaurav Honnavara <gauravhm96@gmail.com>
2022-01-25 01:10:38 +0100

    Changed StringGetPath

--- MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/Program.cs
+++ MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/Program.cs
@@ -19,18 +19,18 @@ namespace MultiSequenceLearning
 
             Console.WriteLine("HELLO!!! Please Select Experiment To Begin:");
 
-            Console.WriteLine("2) Predict Anti Cancer_V1 Peptides Sequences class || ***HTM***");
-            Console.WriteLine("3) Predict Anti Cancer_V2 Peptides Sequences class || ***HTM***");
+            Console.WriteLine("1) Predict Anti Cancer_V1 Peptides Sequences class || ***HTM***");
+            Console.WriteLine("2) Predict Anti Cancer_V2 Peptides Sequences class || ***HTM***");
 
             Console.WriteLine("Please Enter Experimnt Number To Begin the Experiment");
             var selectedExperiment = Console.ReadLine();
 
-            if (selectedExperiment == "2")
+            if (selectedExperiment == "1")
             {
                 Console.WriteLine("-------------INITIATING CANCER SEQUENCE CLASSIFICATION_v1 EXPERIMENT || ***HTM  ***-------------");
                 experimentHTM.InitiateCancerSequenceClassification();
             }
-            else if (selectedExperiment == "3")
+            else if (selectedExperiment == "2")
             {
                 Console.WriteLine("-------------INITIATING CANCER SEQUENCE CLASSIFICATION_v2 EXPERIMENT || ***HTM  ***-------------");
                 experimentHTM.InitiateCancerSequenceClassificationExperimentV2();
--- MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/SequenceLearning_HTM.cs
+++ MyProjectWork/MultiSequenceLearning/MultiSequenceLearning/SequenceLearning_HTM.cs
@@ -17,7 +17,10 @@ namespace MultiSequenceLearning
     {
         static readonly int numColumns = 2048;
 
-        static readonly string LungCancerSequenceDataFile = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory + @"Final Project\Sequence\Anticancer_Peptides\ACPs_Lung_cancer.csv");
+       // static readonly string LungCancerSequenceDataFile = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory + @"Final Project\Sequence\Anticancer_Peptides\ACPs_Lung_cancer.csv");
+        static readonly string LungCancerSequenceDataFile = @"F:\Frankfurt University Of Applied Sciences\Software Engineering(VS)\Final Project\Sequence\Anticancer_Peptides\ACPs_Breast_cancer.csv";
+      // @"F:\Frankfurt University Of Applied Sciences\Software Engineering(VS)\Final Project\Sequence\Anticancer_Peptides\ACPs_Lung_cancer.csv";
+
         static readonly string BreastCancerSequenceDataFile = Path.GetFullPath(System.AppDomain.CurrentDomain.BaseDirectory + @"Final Project\Sequence\Anticancer_Peptides\ACPs_Breast_cancer.csv");
 
         public void InitiateCancerSequenceClassification()
