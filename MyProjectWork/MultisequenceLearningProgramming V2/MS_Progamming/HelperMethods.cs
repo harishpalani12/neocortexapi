3bd00b3014679c30e3b389529371a164b53ee9e3
Harish Palanivel <harishpalani12@gmail.com>
2022-01-26 20:34:20 +0530

    Code correction

--- MyProjectWork/MultisequenceLearningProgramming V2/MS_Progamming/HelperMethods.cs
+++ MyProjectWork/MultisequenceLearningProgramming V2/MS_Progamming/HelperMethods.cs
@@ -20,51 +20,7 @@ namespace ThesisExperiments
         static readonly string[] CancerSequenceClasses = new string[] { "inactive - exp", "mod. active", "very active", "inactive - virtual" };
         static readonly float[][] CancerSequenceClassesOneHotEncoding = new float[][] { new float[] { 0, 0, 0, 1 }, new float[] { 0, 0, 1, 0 }, new float[] { 0, 1, 0, 0 }, new float[] { 1, 0, 0, 0 } };
 
-        // READING TRAINING DATA
-        /// <summary>
-        ///     Fetch Taxi Data from file
-        /// </summary>
-        /// <param name="dataFilePath">Training Data File Path</param>
-        /// <returns></returns>
-        public static List<Dictionary<string, string>> ReadPassengerDataFromFile(string dataFilePath)
-        {
-            List<Dictionary<string, string>> SequencesCollection = new List<Dictionary<string, string>>();
-
-            int keyForUniqueIndexes = 0;
-
-            if (File.Exists(dataFilePath))
-            {
-
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
-                    Dictionary<string, string> Sequence = new Dictionary<string, string>();
-                    while (sr.Peek() >= 0)
-                    {
-                        var line = sr.ReadLine();
-                        string[] values = line.Split(",");
-
-
-                        keyForUniqueIndexes++;
-
-                        var observationPassengerCount = values[1];
-                        var observationDateTime = values[0];
-
-                        if (Sequence.ContainsKey(values[1]))
-                        {
-                            var newKey = values[1] + "," + keyForUniqueIndexes;
-                            Sequence.Add(newKey, observationDateTime);
-                        }
-                        else
-                        {
-                            Sequence.Add(observationPassengerCount, observationDateTime);
-                        }
-                    }
-                    SequencesCollection.Add(Sequence);
-                }
-                return SequencesCollection;
-            }
-            return null;
-        }
+       
         /// <summary>
         ///     Fetch Cancer Peptides data from file
         /// </summary>
@@ -93,7 +49,6 @@ namespace ThesisExperiments
                         string label = values[1];
                         string sequenceString = values[0];
 
-                        int count = 0;
 
                         foreach (var alphabet in sequenceString)
                         {
@@ -116,95 +71,12 @@ namespace ThesisExperiments
 
                 return SequencesCollection;
             }
-            return null;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="dataFilePath"></param>
-        /// <returns></returns>
-        public static Dictionary<float[][], float[][]> ReadCancerSequencesDataFromFile_LSTM_v2(string dataFilePath)
-        {
-            List<Dictionary<string, string>> SequencesCollection = new List<Dictionary<string, string>>();
-
-            int keyForUniqueIndexes = 0;
-
-            if (File.Exists(dataFilePath))
+            else
             {
-
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        var line = sr.ReadLine();
-                        string[] values = line.Split(",");
-
-                        Dictionary<string, string> Sequence = new Dictionary<string, string>();
-                        int count = 0;
-
-                        string label = values[1];
-                        string sequenceString = values[0];
-
-                        if (sequenceString.Length < 33)
-                        {
-                            int remainingLength = 33 - sequenceString.Length;
-                            for (int i = 0; i < remainingLength; i++)
-                            {
-                                sequenceString = sequenceString + "Z";
-                            }
-                        }
-                        Sequence.Add(sequenceString, label);
-                        SequencesCollection.Add(Sequence);
-                    }
-                }
-
-                float[][] processedDataSet = new float[SequencesCollection.Count][];
-                float[][] processedLabel = new float[SequencesCollection.Count][];
-
-                int index = 0;
-                foreach (var sequence in SequencesCollection)
-                {
-
-                    var sequenceDict = sequence;
-                    var sequenceString = sequenceDict.Keys.ElementAt(0);
-                    var sequenceLabel = sequenceDict.Values.ElementAt(0).Split("_")[0];
-
-                    float[] sequenceProcessed = new float[0];
-
-                    foreach (var element in sequenceString)
-                    {
-                        var numericval = char.ToUpper(element) - 64;
-                        sequenceProcessed = sequenceProcessed.Concat(new float[] { numericval }).ToArray();
-                    }
-
-                    processedDataSet[index] = sequenceProcessed;
-                    var label_encoded = new float[0];
-                    if (sequenceLabel == CancerSequenceClasses[0])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[0];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[1])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[1];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[2])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[2];
-                    }
-                    else if (sequenceLabel == CancerSequenceClasses[3])
-                    {
-                        label_encoded = CancerSequenceClassesOneHotEncoding[3];
-                    }
-                    processedLabel[index] = label_encoded;
-                    index++;
-                }
-                var returnDict = new Dictionary<float[][], float[][]>();
-                returnDict.Add(processedDataSet, processedLabel);
-
-                return returnDict;
+                return null;
             }
-            return null;
         }
+       
         /// <summary>
         /// 
         /// </summary>
@@ -256,57 +128,7 @@ namespace ThesisExperiments
             return null;
         }
 
-        //*********************************************************************************************
-        // ENCODING TRAINING DATA
-        /// <summary>
-        ///     Encode Taxi Passenger Data
-        /// </summary>
-        /// <param name="trainingData"></param>
-        /// <returns></returns>
-        public static List<Dictionary<string, int[]>> EncodePassengerData(List<Dictionary<string, string>> trainingData)
-        {
-            List<Dictionary<string, int[]>> ListOfEncodedTrainingSDR = new List<Dictionary<string, int[]>>();
-
-            ScalarEncoder DayEncoder = FetchDayEncoder();
-            ScalarEncoder MonthEncoder = FetchMonthEncoder();
-            ScalarEncoder YearEncoder = FetchYearEncoder();
-            ScalarEncoder DayOfWeekEncoder = FetchWeekDayEncoder();
-
-            foreach (var sequence in trainingData)
-            {
-
-                var tempDictionary = new Dictionary<string, int[]>();
-
-                foreach (var keyValuePair in sequence)
-                {
-
-                    var observationLabel = keyValuePair.Key;
-                    var element = keyValuePair.Value;
-
-                    DateTime observationDateTime = DateTime.Parse(element);
-                    int day = observationDateTime.Day;
-                    int month = observationDateTime.Month;
-                    int year = observationDateTime.Year;
-                    int dayOfWeek = (int)observationDateTime.DayOfWeek;
-
-                    int[] sdr = new int[0];
-
-                    sdr = sdr.Concat(DayEncoder.Encode(day)).ToArray();
-                    sdr = sdr.Concat(MonthEncoder.Encode(month)).ToArray();
-                    sdr = sdr.Concat(YearEncoder.Encode(year)).ToArray();
-                    sdr = sdr.Concat(DayOfWeekEncoder.Encode(dayOfWeek)).ToArray();
-
-                    //    UNCOMMENT THESE LINES TO DRAW SDR BITMAP
-                    //    int[,] twoDimenArray = ArrayUtils.Make2DArray<int>(sdr, 100, 100);
-                    //    var twoDimArray = ArrayUtils.Transpose(twoDimenArray);
-                    //    NeoCortexUtils.DrawBitmap(twoDimArray, 1024, 1024, $"{observationDateTime.Day.ToString()}.png", null);
-                    tempDictionary.Add(observationLabel, sdr);
-                }
-                ListOfEncodedTrainingSDR.Add(tempDictionary);
-            }
-            return ListOfEncodedTrainingSDR;
-        }
-
+       
         /// <summary>
         ///     Encoding Amino Alphabetic Sequences
         /// </summary>
@@ -440,32 +262,7 @@ namespace ThesisExperiments
             return null;
         }
 
-        public static Dictionary<float[], float[][]> PassengerCountDataProcessing(Dictionary<string, string> trainingData)
-        {
-            var processedData = new float[trainingData.Count][];
-            var obesrvationLabel = new float[trainingData.Count];
-            var returnDictionary = new Dictionary<float[], float[][]>();
-
-            int index = 0;
-            foreach (var element in trainingData)
-            {
-                var observationDateTime = DateTime.Parse(element.Value);
-                var observationLabel = element.Key;
-
-                var dayOfWeek = (int)observationDateTime.DayOfWeek;
-                var day = observationDateTime.Day;
-                var month = observationDateTime.Month;
-                var year = observationDateTime.Year;
-
-                var processedDatarow = new float[] { day, month, year, dayOfWeek };
-                processedData[index] = processedDatarow;
-                obesrvationLabel[index] = float.Parse(observationLabel) / 73739; // DATA NORMALIZATION
-                index++;
-            }
-
-            returnDictionary.Add(obesrvationLabel, processedData);
-            return returnDictionary;
-        }
+       
 
         //*********************************************************************************************
         // HTM HELPER METHODS
@@ -529,27 +326,7 @@ namespace ThesisExperiments
         /// <summary>
         ///         ENCODE USER INPUT 
         /// </summary>
-        public static int[] EncodeSingleInput_testingExperiment_1(string userInput)
-        {
-
-            DateTime date = DateTime.Parse(userInput);
-            var day = date.Day;
-            var month = date.Month;
-            var year = date.Year;
-            var weekDay = (int)date.DayOfWeek;
-
-            ScalarEncoder DayEncoder = FetchDayEncoder();
-            ScalarEncoder MonthEncoder2 = FetchMonthEncoder();
-            ScalarEncoder YearEncoder3 = FetchYearEncoder();
-            ScalarEncoder WeekDayEncoder = FetchWeekDayEncoder();
-
-            int[] sdr = new int[0];
-            sdr = sdr.Concat(DayEncoder.Encode(day)).ToArray();
-            sdr = sdr.Concat(MonthEncoder2.Encode(month)).ToArray();
-            sdr = sdr.Concat(YearEncoder3.Encode(year)).ToArray();
-            sdr = sdr.Concat(WeekDayEncoder.Encode(weekDay)).ToArray();
-            return sdr;
-        }
+       
         public static List<int[]> EncodeSingleInput_testingExperiment_2(string userInput, Boolean EncodeSingleAlphabet)
         {
 
@@ -599,63 +376,7 @@ namespace ThesisExperiments
         ///         FETCH ENCODERS 
         /// </summary>
         /// <returns> SCALAR ENCODERS</returns>
-        public static ScalarEncoder FetchWeekDayEncoder()
-        {
-            ScalarEncoder MonthEncoder = new ScalarEncoder(new Dictionary<string, object>()
-            {
-                { "W", 3},
-                { "N", 11},
-                { "MinVal", (double)0}, // Min value = (0).
-                { "MaxVal", (double)7}, // Max value = (7).
-                { "Periodic", true}, // Since Monday would repeat again.
-                { "Name", "WeekDay"},
-                { "ClipInput", true},
-            });
-            return MonthEncoder;
-        }
-        public static ScalarEncoder FetchDayEncoder()
-        {
-            ScalarEncoder DayEncoder = new ScalarEncoder(new Dictionary<string, object>()
-            {
-                { "W", 3},
-                { "N", 35},
-                { "MinVal", (double)1}, // Min value = (0).
-                { "MaxVal", (double)32}, // Max value = (7).
-                { "Periodic", true},
-                { "Name", "Date"},
-                { "ClipInput", true},
-           });
-
-            return DayEncoder;
-        }
-        public static ScalarEncoder FetchMonthEncoder()
-        {
-            ScalarEncoder MonthEncoder = new ScalarEncoder(new Dictionary<string, object>()
-            {
-                { "W", 3},
-                { "N", 15},
-                { "MinVal", (double)1}, // Min value = (0).
-                { "MaxVal", (double)12}, // Max value = (7).
-                { "Periodic", true}, // Since Monday would repeat again.
-                { "Name", "Month"},
-                { "ClipInput", true},
-            });
-            return MonthEncoder;
-        }
-        public static ScalarEncoder FetchYearEncoder()
-        {
-            ScalarEncoder YearEncoder = new ScalarEncoder(new Dictionary<string, object>()
-            {
-                { "W", 3},
-                { "N", 11},
-                { "MinVal", (double)2018}, // Min value = (2018).
-                { "MaxVal", (double)2022}, // Max value = (2021).
-                { "Periodic", false}, // Since Monday would repeat again.
-                { "Name", "Year"},
-                { "ClipInput", true},
-            });
-            return YearEncoder;
-        }
+        
         public static ScalarEncoder FetchAlphabetEncoder()
         {
             ScalarEncoder AlphabetEncoder = new ScalarEncoder(new Dictionary<string, object>()
@@ -674,84 +395,6 @@ namespace ThesisExperiments
         //************************************************************************************************
 
         //*************************TESTING METHODS*******************************************
-
-        /// <summary>
-        /// FOR PASSENGER COUNT PREDICTION EXPERIMENT
-        /// </summary>
-        /// <param name="trainingData">TRAINING DATA</param>
-        /// <param name="trained_CortexLayer">TRAINED HTM CORTEX LAYER</param>
-        /// <param name="trained_Classifier">TRAINED HTM CLASSIFIER</param>
-        public static void BeginAutomatedTestingExperiment_1(List<Dictionary<string, string>> trainingData, CortexLayer<object, object> layer, HtmClassifier<string, ComputeCycle> cls)
-        {
-            //** We will use 40% of trainingData for tesing.
-            Random rnd = new Random();
-
-            var testingResults = new List<string>();
-
-            var numberOfDataPoints = (int)(0.7 * trainingData[0].Count);
-            int correctPrediction = 0;
-
-            //FORMULAE -> Summation[Predictied-true]/numberOfDataPoints
-            double MeanAbsoluteError = 0.0;
-
-
-            for (int i = 0; i < numberOfDataPoints; i++)
-            {
-                var index = rnd.Next(1, trainingData[0].Count - 1); // WE ARE USING RANGE FROM 1 because FIRST ELEMENT OF SEQUENCE CANNOT BE PREDICTED *******
-                var sequenceElement = trainingData[0].ElementAt(index);
-
-                var testInput = sequenceElement.Value;
-                var testLabel = trainingData[0].ElementAt(index + 1).Key;
-
-                var sdr = EncodeSingleInput_testingExperiment_1(testInput);
-                var lyrOutput = layer.Compute(sdr, false) as ComputeCycle;
-                var predictedValues = cls.GetPredictedInputValues(lyrOutput.PredictiveCells.ToArray(), 3);
-                Boolean IsPredictionCorrect = false;
-                foreach (var prediction in predictedValues)
-                {
-                    testingResults.Add($"Actual Value : {testLabel} \t Predicted Value:{prediction.PredictedInput}");
-                    Console.WriteLine($"Actual Value : {testLabel} \t Predicted Value:{prediction.PredictedInput}");
-                    if (prediction.PredictedInput == testLabel)
-                    {
-                        IsPredictionCorrect = true;
-                        break;
-                    }
-                    else
-                    {
-                        MeanAbsoluteError += (double.Parse(prediction.PredictedInput) - double.Parse(testLabel));
-                    }
-                }
-
-
-                if (IsPredictionCorrect)
-                {
-                    correctPrediction++;
-                }
-
-            }
-            if (MeanAbsoluteError < 0) { MeanAbsoluteError = 0 - MeanAbsoluteError; }
-            MeanAbsoluteError = MeanAbsoluteError / trainingData[0].Count;
-            Console.WriteLine($"Mean Absolute Error : {MeanAbsoluteError}");
-
-            double accuracy = ((double)correctPrediction / numberOfDataPoints) * 100;
-            Console.WriteLine($"AUTOMATED TESTING COMPLELETED");
-            Console.WriteLine($"ACCURACY OBTAINED: {accuracy}");
-            testingResults.Add($"ACCURACY OBTAINED: {accuracy}");
-
-            DateTime now = DateTime.Now;
-            long n = long.Parse(now.ToString("yyyyMMddHHmmss"));
-            string filename = "CancerSequenceAutomatedTesting" + now.ToString("g").Split(" ")[0] + n + ".txt";
-            string path = System.AppDomain.CurrentDomain.BaseDirectory + "\\TestingLogs\\" + filename;
-            using (StreamWriter swOutput = File.CreateText(path))
-            {
-                swOutput.WriteLine($"{filename}");
-                foreach (var result in testingResults)
-                {
-                    swOutput.WriteLine(result);
-                }
-            }
-
-        }
 
         /// <summary>
         ///  CANCER SEQUENCE CLASSIFICATION EXPERIMENT V1
--- MyProjectWork/MultisequenceLearningProgramming V2/MS_Progamming/Program.cs
+++ MyProjectWork/MultisequenceLearningProgramming V2/MS_Progamming/Program.cs
@@ -15,26 +15,23 @@ namespace ThesisExperiments
         {
 
 
-            /// <summary>
-            /// Experiment-1  [A] HTM  [B] LSTM :::::----Taxi Passenger Count Prediction 1 timestamp in advance i.e 1 day in advance
-            /// </summary>
 
             /// <summary>
             /// Experiment-2  [A] HTM  [B] LSTM :::::----Cancer Sequence Classification i.e classify sequence in 4 Categories i.e Mod. Active , InActive, Very Active, Virtually Acitve
             /// </summary>
 
             SequenceLearningHTM experimentHTM = new SequenceLearningHTM();
-            SequenceLearningLSTM experimentLSTM = new SequenceLearningLSTM();
+            //SequenceLearningLSTM experimentLSTM = new SequenceLearningLSTM();
 
             Console.WriteLine("HELLO!!! Please Select Experiment To Begin:");
 
             //-----------------------------------HTM-----------------------------------
-            Console.WriteLine("1) Predict Passenger Demand {NYC taxi Dataset used for Training || ***HTM***");
+           // Console.WriteLine("1) Predict Passenger Demand {NYC taxi Dataset used for Training || ***HTM***");
             Console.WriteLine("2) Predict Anti Cancer_V1 Peptides Sequences class || ***HTM***");
             Console.WriteLine("3) Predict Anti Cancer_V2 Peptides Sequences class || ***HTM***");
 
             //-----------------------------------LSTM-----------------------------------
-            Console.WriteLine("4) Predict Passenger Demand {NYC taxi Dataset used for Training || ***LSTM***");
+           // Console.WriteLine("4) Predict Passenger Demand {NYC taxi Dataset used for Training || ***LSTM***");
             Console.WriteLine("5) Predict Anti Cancer_V1 Peptides Sequences class || ***LSTM***");
             Console.WriteLine("6) Predict Anti Cancer_V2 Peptides Sequences class || ***LSTM***");
 
@@ -43,15 +40,9 @@ namespace ThesisExperiments
 
             //**************************************************************************
             //                               HTM
-            if (selectedExperiment == "1")
+            if (selectedExperiment == "2")
             {
 
-                Console.WriteLine("-------------INITIATING PREDICT PASSENGER COUNT PREDICTION EXPERIMENT || ***HTM***-------------");
-                experimentHTM.InitiatePassengerCountPredictionExperiment();
-
-            }
-            else if (selectedExperiment == "2")
-            {
                 Console.WriteLine("-------------INITIATING CANCER SEQUENCE CLASSIFICATION_v1 EXPERIMENT || ***HTM  ***-------------");
                 experimentHTM.InitiateCancerSequenceClassificationExperiment();
             }
@@ -63,21 +54,6 @@ namespace ThesisExperiments
             //**************************************************************************
             //                               LSTM
 
-            else if (selectedExperiment == "4")
-            {
-                Console.WriteLine("-------------INITIATING PREDICT PASSENGER COUNT PREDICTION EXPERIMENT || ***LSTM***-------------");
-                experimentLSTM.InitiatePassengerCountPredictionExperiment();
-            }
-            else if (selectedExperiment == "5")
-            {
-                Console.WriteLine("-------------INITIATING CANCER SEQUENCE CLASSIFICATION_v1 EXPERIMENT || ***LSTM***-------------");
-                experimentLSTM.InitiateCancerSequenceClassificationExperimentV1();
-            }
-            else if (selectedExperiment == "6")
-            {
-                Console.WriteLine("-------------INITIATING CANCER SEQUENCE CLASSIFICATION_v2 EXPERIMENT || ***LSTM***-------------");
-                experimentLSTM.InitiateCancerSequenceClassificationExperimentV2();
-            }
 
             //**************************************************************************
             else
