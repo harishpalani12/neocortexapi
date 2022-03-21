39a07dc481b5d5e4f4eff8ed3f1626e523a3d247
Harish Palanivel <harishpalani12@gmail.com>
2022-03-21 13:00:57 +0530

    Code moved to MySEProject

---
+++ source/MySEProject/SimpleMultiSequenceLearning/MyHelperMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Utility;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Classifiers;
+using NeoCortexApi.Network;
+
+using HtmImageEncoder;
+
+using Daenet.ImageBinarizerLib.Entities;
+using Daenet.ImageBinarizerLib;
+
+namespace SimpleMultiSequenceLearning
+{
+    /// <summary>
+    /// Helper Method For MultiSequence Learning
+    /// </summary>
+    public class MyHelperMethod
+    {
+        static readonly string[] SequenceClasses = new string[] { "inactive - exp", "mod. active", "very active", "inactive - virtual" };
+        static readonly float[][] SequenceClassesOneHotEncoding = new float[][] { new float[] { 0, 0, 0, 1 }, new float[] { 0, 0, 1, 0 }, new float[] { 0, 1, 0, 0 }, new float[] { 1, 0, 0, 0 } };
+
+    }
+}
